8f1d6333942d01dcf80d11f462182a07878d0e9f
chester669 <viktorostash44@gmail.com>
2024-06-25 19:34:34 +0300

    delete buttons

--- src/js/main-page.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    window.location.href = button.getAttribute('data-href');
-  });
-});
